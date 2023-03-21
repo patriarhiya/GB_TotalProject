44c3a6fb40db0eeb964ce70c42bc3c81060a1efd
patriarhiya <patriarhiya@mail.ru>
2023-03-21 22:12:20 +0300

    Добавил программу

---
+++ Program/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using static System.Console;
+using static System.Array;
+
+string [] array = {"привет", "22", "опа", "хорошо", "-2"};
+int count = 0;
+string [] newArray = array;
+
+for (int i = 0; i < array.Length; i++)
+{
+    if(array[i].Length < 4){
+        newArray[count] = array[i];
+        count++;
+    }
+}
+
+Resize(ref newArray, count);
+foreach (var item in newArray) Write($"{item} ");
